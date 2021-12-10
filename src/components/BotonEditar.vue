a6a4f04963a6060d943059722fc51eef68784b01
DouglasGuacaran <douglasguacaran@gmail.com>
2021-12-09 22:30:24 -0300

    Creo boton editar

---
+++ src/components/BotonEditar.vue
@@ -0,0 +1,14 @@
+<template>
+  <b-button>Editar</b-button>
+</template>
+
+<script>
+export default {
+    name:'BotonEditar'
+
+}
+</script>
+
+<style>
+
+</style>
--- src/components/CardHero.vue
+++ src/components/CardHero.vue
@@ -5,12 +5,17 @@
         <b-card-text> Nombre del Heroe: {{ nombre }} </b-card-text>
         <b-card-text> Descripcion: {{ descripcion }} </b-card-text>
         <b-card-text> Modificado: {{ modificado }} </b-card-text>
+        <Boton/>
       </b-card>
 </template>
 
 <script>
+import Boton from '../components/BotonEditar.vue'
 export default {
   name: "Card",
+  components:{
+    Boton
+  },
   props: {
     nombre: String,
     thumbnailPath: String,
